9125080acbf0300e56da6c170e1f016797b1ac2e
Miika Kontio <miikako89@gmail.com>
2021-02-14 13:58:47 +0200

    GameOptionsiin lisätty FIRSTSTATE jolla voi määrittää mistä kentästä peli alkaa

--- PottumaaJava/src/Main/GameOptions.java
+++ PottumaaJava/src/Main/GameOptions.java
@@ -4,6 +4,16 @@ public class GameOptions {
 
     public static boolean ISDEBUGMODE = false;
 
+    /*
+     MENUSTATE = 0;
+	 LEVEL1STATE = 1;
+	 LEVEL2STATE = 2;
+	 HELPSTATE = 3;
+	 OPTIONSSTATE = 4;
+	 MAPEDITORSTATE = 5;
+    */
+    public static int FIRSTSTATE = 0;
+
     public static void ToggleDebugMode() {
         ISDEBUGMODE = !ISDEBUGMODE;
     }
