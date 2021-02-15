f4fc0836fecf3f992c00236975fd8c3c91877817
Miika Kontio <miika.kontio@innofactor.com>
2021-02-16 00:09:44 +0200

    Questin läpäisemiselle ääniefekti

--- PottumaaJava/src/Entity/Quests/Quest.java
+++ PottumaaJava/src/Entity/Quests/Quest.java
@@ -1,18 +1,28 @@
 package Entity.Quests;
 
+import Audio.AudioPlayer;
 import Entity.Player.Player;
+
+import java.util.HashMap;
 
 public abstract class Quest {
 
     private String title;
     private String description;
-
+    private HashMap<String, AudioPlayer> sfx;
     private boolean questIsDone;
 
     public Quest(String title, String description) {
         this.title = title;
         this.description = description;
         questIsDone = false;
+
+        setSoundEffects();
+    }
+
+    private void setSoundEffects() {
+        sfx = new HashMap<>();
+        sfx.put("questIsDone", new AudioPlayer("/SFX/questIsDone.wav"));
     }
 
     public String getTitle() {
@@ -29,8 +39,15 @@ public abstract class Quest {
 
     public void markQuestDone(Player player) {
         questIsDone = true;
+        playSoundEffect("questIsDone");
+        //reward player for finishing quest
+        //todo: add rewardMoneyAmount and rewardExperienceAmount to Quest, so that every quest gives different amount of rewards
         player.addMoney(100);
         player.addExperience(100);
+    }
+
+    private void playSoundEffect(String soundEffectName) {
+        sfx.get(soundEffectName).play();
     }
 
     public void KillOneEnemy(int EnemyType, Player player) {
