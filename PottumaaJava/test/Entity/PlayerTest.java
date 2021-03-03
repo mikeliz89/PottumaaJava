7d25419fed47ea15c7a04fb17e8914def96aacfe
Miika Kontio <miika.kontio@innofactor.com>
2021-03-03 13:08:03 +0200

    JUnit unit testien korjaukset

--- PottumaaJava/test/Entity/PlayerTest.java
+++ PottumaaJava/test/Entity/PlayerTest.java
@@ -2,6 +2,8 @@ package Entity;
 
 import Entity.Obstacles.Obstacle;
 import Entity.Player.Player;
+import Entity.Player.PlayerSettings;
+import GameState.ISaveManager;
 import TileMap.TileMap;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +13,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
 
+    //Todo: Lisää joku Mocking -library, esim EasyMock ja toteuta sillä saveManager.load -metodi jotta testataan myös player.load
+    private ISaveManager saveManager;
     private final ArrayList<TileMap> myTileMaps;
     private final ArrayList<Obstacle> myObstacles;
     Player myPlayer;
@@ -18,31 +22,31 @@ public class PlayerTest {
     public PlayerTest() {
         myTileMaps = new ArrayList<>();
         myObstacles = new ArrayList<>();
-        myPlayer = new Player(myTileMaps, myObstacles);
+        myPlayer = new Player(myTileMaps, myObstacles, saveManager);
     }
 
     @Test
     void getHealth() {
         var health = myPlayer.getHealth();
-        assertEquals(5, health);
+        assertEquals(PlayerSettings.PLAYER_START_HEALTH, health);
     }
 
     @Test
     void getMaxHealth() {
         var maxHealth = myPlayer.getMaxHealth();
-        assertEquals(5, maxHealth);
+        assertEquals(PlayerSettings.PLAYER_START_HEALTH, maxHealth);
     }
 
     @Test
     void WhenPlayerGetsHitZeroDamageHealthShouldBeFive() {
         myPlayer.hit(0);
         var health = myPlayer.getHealth();
-        assertEquals(5, health);
+        assertEquals(PlayerSettings.PLAYER_START_HEALTH, health);
     }
 
     @Test
     void WhenPlayerGetsHitDamageMoreThanMaxHealthPlayerHealthShouldBeZero() {
-        myPlayer.hit(6);
+        myPlayer.hit(PlayerSettings.PLAYER_START_HEALTH+1);
         var health = myPlayer.getHealth();
         assertEquals(0, health);
     }
--- PottumaaJava/test/Entity/TileMapTest.java
+++ PottumaaJava/test/Entity/TileMapTest.java
@@ -27,30 +27,30 @@ public class TileMapTest {
     @Test
     public void getTileTypeDoesGiveGoodValueOnRowZeroColumnZero() {
         var tileType = myTileMap.getTileType(0, 0);
-        assertEquals(1, tileType);
+        assertEquals(Tile.TILE_FRICTION_TYPE_GRASS, tileType);
     }
 
     @Test
     public void getTileTypeDoesGiveGoodValueOnRowTenColumnTen() {
         var tileType = myTileMap.getTileType(10, 10);
-        assertEquals(0, tileType);
+        assertEquals(Tile.TILE_FRICTION_TYPE_GRASS, tileType);
     }
 
     @Test
     public void getTileTypeDoesGiveGoodValueOnRTooSmallRow() {
         var tileType = myTileMap.getTileType(-1, 0);
-        assertEquals(1, tileType);
+        assertEquals(Tile.TILE_FRICTION_TYPE_GRASS, tileType);
     }
 
     @Test
     public void getTileTypeDoesNotThrowExceptionOnTooBigRow() {
         var tileType = myTileMap.getTileType(31, 10);
-        assertEquals(1, tileType);
+        assertEquals(Tile.TILE_FRICTION_TYPE_GRASS, tileType);
     }
 
     @Test
     public void getTileTypeDoesNotThrowExceptionOnTooBigColumn() {
         var tileType = myTileMap.getTileType(10, 31);
-        assertEquals(1, tileType);
+        assertEquals(Tile.TILE_FRICTION_TYPE_GRASS, tileType);
     }
 }
